a7e46bfbbb213c674deff4766c122ffe967dad86
Ruud Puts <ruud.puts@gmail.com>
2020-04-04 11:26:22 +0200

    Fix class not found exception in file_updater_spec

--- cocoapods/spec/dependabot/cocoapods/file_updater_spec.rb
+++ cocoapods/spec/dependabot/cocoapods/file_updater_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require "dependabot/dependency"
+require "dependabot/dependency_file"
 require "dependabot/cocoapods/file_updater"
 require_common_spec "file_updaters/shared_examples_for_file_updaters"
 
