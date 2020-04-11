41f9f7d4062906536b4aa9e10177e37b6b36c0b8
Ruud Puts <ruud.puts@gmail.com>
2020-04-11 17:18:52 +0200

    Rubocop fixes

--- cocoapods/spec/dependabot/cocoapods/file_updater_spec.rb
+++ cocoapods/spec/dependabot/cocoapods/file_updater_spec.rb
@@ -21,49 +21,62 @@ RSpec.describe Dependabot::CocoaPods::FileUpdater do
     fixture("cocoapods", "CocoaPods-Version.yml")
   end
 
+  let(:fixtures_path) do
+    File.join("spec", "fixtures", "cocoapods")
+  end
+
+  let(:all_versions_files) do
+    File.join(fixtures_path, "all_pods", "all_pods_versions_*.txt")
+  end
+
+  let(:podspec_files) do
+    File.join(fixtures_path, "podspecs", "*.podspec.json")
+  end
+
   def stub_all_pods_versions_requests
-    all_versions_files = File.join("spec", "fixtures", "cocoapods", "all_pods", "all_pods_versions_*.txt")
     Dir[all_versions_files].each do |file|
       versions_url = "#{COCOAPODS_CDN_HOST}/#{File.basename(file)}"
       stub_request(:get, versions_url).
-          to_return(status: 200, body: File.read(file))
+        to_return(status: 200, body: File.read(file))
     end
   end
 
   def stub_all_spec_requests
     spec_paths = {
-        "Nimble-0.0.1": "Specs/d/c/d/Nimble/0.0.1/Nimble.podspec.json",
-        "Alamofire-3.0.1": "Specs/d/a/2/Alamofire/3.0.1/Alamofire.podspec.json",
-        "Alamofire-3.5.1": "Specs/d/a/2/Alamofire/3.5.1/Alamofire.podspec.json",
-        "Alamofire-4.5.0": "Specs/d/a/2/Alamofire/4.5.0/Alamofire.podspec.json",
-        "Alamofire-4.5.1": "Specs/d/a/2/Alamofire/4.5.1/Alamofire.podspec.json",
-        "Alamofire-4.6.0": "Specs/d/a/2/Alamofire/4.6.0/Alamofire.podspec.json",
-        "AlamofireImage-2.5.0": "Specs/8/0/a/AlamofireImage/2.5.0/AlamofireImage.podspec.json",
-        "AlamofireImage-4.1.0": "Specs/8/0/a/AlamofireImage/4.1.0/AlamofireImage.podspec.json"
+      "Nimble-0.0.1": "Specs/d/c/d/Nimble/0.0.1/Nimble.podspec.json",
+      "Alamofire-3.0.1": "Specs/d/a/2/Alamofire/3.0.1/Alamofire.podspec.json",
+      "Alamofire-3.5.1": "Specs/d/a/2/Alamofire/3.5.1/Alamofire.podspec.json",
+      "Alamofire-4.5.0": "Specs/d/a/2/Alamofire/4.5.0/Alamofire.podspec.json",
+      "Alamofire-4.5.1": "Specs/d/a/2/Alamofire/4.5.1/Alamofire.podspec.json",
+      "Alamofire-4.6.0": "Specs/d/a/2/Alamofire/4.6.0/Alamofire.podspec.json",
+      "AlamofireImage-2.5.0":
+          "Specs/8/0/a/AlamofireImage/2.5.0/AlamofireImage.podspec.json",
+      "AlamofireImage-4.1.0":
+          "Specs/8/0/a/AlamofireImage/4.1.0/AlamofireImage.podspec.json"
     }
 
-    Dir[File.join("spec", "fixtures", "cocoapods", "podspecs", "*.podspec.json")].each do |file|
+    Dir[podspec_files].each do |file|
       spec = File.basename(file, ".podspec.json")
       spec_path = spec_paths[spec.to_sym]
       versions_url = "#{COCOAPODS_CDN_HOST}/#{spec_path}"
 
       stub_request(:get, versions_url).
-          to_return(status: 200, body: File.read(file))
+        to_return(status: 200, body: File.read(file))
     end
   end
 
   before do
     all_pods_url = "#{COCOAPODS_CDN_HOST}/all_pods.txt"
     stub_request(:get, all_pods_url).
-        to_return(status: 200, body: cocoapods_all_pods)
+      to_return(status: 200, body: cocoapods_all_pods)
 
     deprecated_specs_url = "#{COCOAPODS_CDN_HOST}/deprecated_podspecs.txt"
     stub_request(:get, deprecated_specs_url).
-        to_return(status: 200, body: cocoapods_deprecated_specs)
+      to_return(status: 200, body: cocoapods_deprecated_specs)
 
     cocoapods_version_url = "#{COCOAPODS_CDN_HOST}/CocoaPods-version.yml"
     stub_request(:get, cocoapods_version_url).
-        to_return(status: 200, body: cocoapods_version_yaml)
+      to_return(status: 200, body: cocoapods_version_yaml)
 
     stub_all_pods_versions_requests
     stub_all_spec_requests
