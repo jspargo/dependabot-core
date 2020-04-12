ed8ca381ca9b253cbfbd8b763ffb56f6d4df621e
Ruud Puts <ruud.puts@gmail.com>
2020-04-12 14:13:08 +0200

    Add missing fixtures for update_checker_spec (#3)
    * Added all missing fixtures to make update_checker_spec run
    
    * Fix merge conflict
    
    * revert a file
    
    * Fix file_updater_spec
    
    * Fixing file_updater_spec
    
    * Update cocoapods/spec/dependabot/cocoapods/update_checker_spec.rb
    
    Co-Authored-By: Jack Spargo <jspargo@shopkeep.com>
    
    * Update cocoapods/spec/dependabot/cocoapods/update_checker_spec.rb
    
    Co-Authored-By: Jack Spargo <jspargo@shopkeep.com>
    
    * Update cocoapods/spec/dependabot/cocoapods/update_checker_spec.rb
    
    Co-Authored-By: Jack Spargo <jspargo@shopkeep.com>
    
    * Cleanup podspec fixtures
    
    * All but emptied out all_pods_versions files
    
    * Fixed "uninitialized constant Dependabot::DependencyFile" for file_parser_spec
    
    * Add more Alamofire versions
    
    * Fixed all update_checker_spec tests
    
    * Cleanup
    
    * Rubocop fixes
    
    * Fix file_updater_spec
    
    * Fixed all file_updater tests
    
    Co-authored-by: Jack Spargo <jspargo@shopkeep.com>

--- cocoapods/spec/dependabot/cocoapods/file_updater_spec.rb
+++ cocoapods/spec/dependabot/cocoapods/file_updater_spec.rb
@@ -9,64 +9,85 @@ require_common_spec "file_updaters/shared_examples_for_file_updaters"
 RSpec.describe Dependabot::CocoaPods::FileUpdater do
   it_behaves_like "a dependency file updater"
 
+  COCOAPODS_CDN_HOST = "https://cdn.cocoapods.org"
+
+  let(:cocoapods_all_pods) do
+    fixture("cocoapods", "all_pods", "all_pods.txt")
+  end
+
+  let(:cocoapods_deprecated_specs) do
+    fixture("cocoapods", "podspecs", "deprecated_podspecs.txt")
+  end
+
+  let(:cocoapods_version_yaml) do
+    fixture("cocoapods", "CocoaPods-Version.yml")
+  end
+
+  let(:fixtures_path) do
+    File.join("spec", "fixtures", "cocoapods")
+  end
+
+  let(:all_versions_files) do
+    File.join(fixtures_path, "all_pods", "all_pods_versions_*.txt")
+  end
+
+  let(:podspec_files) do
+    File.join(fixtures_path, "podspecs", "*.podspec.json")
+  end
+
+  def stub_all_pods_versions_requests
+    Dir[all_versions_files].each do |file|
+      versions_url = "#{COCOAPODS_CDN_HOST}/#{File.basename(file)}"
+      stub_request(:get, versions_url).
+        to_return(status: 200, body: File.read(file))
+    end
+  end
+
+  def stub_all_spec_requests
+    spec_paths = {
+      "Nimble-0.0.1": "Specs/d/c/d/Nimble/0.0.1/Nimble.podspec.json",
+      "Nimble-3.0.0": "Specs/d/c/d/Nimble/3.0.0/Nimble.podspec.json",
+      "Alamofire-3.0.1": "Specs/d/a/2/Alamofire/3.0.1/Alamofire.podspec.json",
+      "Alamofire-3.5.1": "Specs/d/a/2/Alamofire/3.5.1/Alamofire.podspec.json",
+      "Alamofire-4.5.0": "Specs/d/a/2/Alamofire/4.5.0/Alamofire.podspec.json",
+      "Alamofire-4.5.1": "Specs/d/a/2/Alamofire/4.5.1/Alamofire.podspec.json",
+      "Alamofire-4.6.0": "Specs/d/a/2/Alamofire/4.6.0/Alamofire.podspec.json",
+      "AlamofireImage-2.5.0":
+          "Specs/8/0/a/AlamofireImage/2.5.0/AlamofireImage.podspec.json",
+      "AlamofireImage-4.1.0":
+          "Specs/8/0/a/AlamofireImage/4.1.0/AlamofireImage.podspec.json"
+    }
+
+    Dir[podspec_files].each do |file|
+      spec = File.basename(file, ".podspec.json")
+      spec_path = spec_paths[spec.to_sym]
+      versions_url = "#{COCOAPODS_CDN_HOST}/#{spec_path}"
+
+      stub_request(:get, versions_url).
+        to_return(status: 200, body: File.read(file))
+    end
+  end
+
+  before do
+    all_pods_url = "#{COCOAPODS_CDN_HOST}/all_pods.txt"
+    stub_request(:get, all_pods_url).
+      to_return(status: 200, body: cocoapods_all_pods)
+
+    deprecated_specs_url = "#{COCOAPODS_CDN_HOST}/deprecated_podspecs.txt"
+    stub_request(:get, deprecated_specs_url).
+      to_return(status: 200, body: cocoapods_deprecated_specs)
+
+    cocoapods_version_url = "#{COCOAPODS_CDN_HOST}/CocoaPods-version.yml"
+    stub_request(:get, cocoapods_version_url).
+      to_return(status: 200, body: cocoapods_version_yaml)
+
+    stub_all_pods_versions_requests
+    stub_all_spec_requests
+  end
+
   before do
     master_url = "https://api.github.com/repos/CocoaPods/Specs/commits/master"
     stub_request(:get, master_url).to_return(status: 304)
-  end
-
-  before do
-    all_pods_body = fixture("cocoapods", "all_pods.txt")
-    all_pods_url = "https://cdn.cocoapods.org/all_pods.txt"
-    stub_request(:get, all_pods_url).
-      to_return(status: 200, body: all_pods_body)
-  end
-
-  before do
-    all_pods_versions_body = fixture("cocoapods", "all_pods_versions_d_a_2.txt")
-    all_pods_versions_url = "https://cdn.cocoapods.org/all_pods_versions_d_a_2.txt"
-    stub_request(:get, all_pods_versions_url).
-      to_return(status: 200, body: all_pods_versions_body)
-  end
-
-  before do
-    cocoapods_version_body = fixture("cocoapods", "CocoaPods-version.yml")
-    cocoapods_version_url = "https://cdn.cocoapods.org/CocoaPods-version.yml"
-    stub_request(:get, cocoapods_version_url).
-      to_return(status: 200, body: cocoapods_version_body)
-  end
-
-  before do
-    deprecated_podspec_body = fixture("cocoapods", "deprecated_podspecs.txt")
-    deprecated_podspec_url = "https://cdn.cocoapods.org/deprecated_podspecs.txt"
-    stub_request(:get, deprecated_podspec_url).
-      to_return(status: 200, body: deprecated_podspec_body)
-  end
-
-  before do
-    alamofire_podspec_body =
-      fixture("cocoapods", "podspecs", "Alamofire-4.5.0.podspec.json")
-    alamofire_podspec_url = "https://cdn.cocoapods.org/Specs/d/a/2/"\
-        "Alamofire/4.5.0/Alamofire.podspec.json"
-    stub_request(:get, alamofire_podspec_url).
-      to_return(status: 200, body: alamofire_podspec_body)
-  end
-
-  before do
-    alamofire_podspec_body =
-      fixture("cocoapods", "podspecs", "Alamofire-4.5.1.podspec.json")
-    alamofire_podspec_url = "https://cdn.cocoapods.org/Specs/d/a/2/"\
-      "Alamofire/4.5.1/Alamofire.podspec.json"
-    stub_request(:get, alamofire_podspec_url).
-      to_return(status: 200, body: alamofire_podspec_body)
-  end
-
-  before do
-    alamofire_podspec_body =
-      fixture("cocoapods", "podspecs", "Alamofire-4.6.0.podspec.json")
-    alamofire_podspec_url = "https://cdn.cocoapods.org/Specs/d/a/2/"\
-      "Alamofire/4.6.0/Alamofire.podspec.json"
-    stub_request(:get, alamofire_podspec_url).
-      to_return(status: 200, body: alamofire_podspec_body)
   end
 
   let(:updater) do
@@ -97,10 +118,10 @@ RSpec.describe Dependabot::CocoaPods::FileUpdater do
   let(:dependency) do
     Dependabot::Dependency.new(
       name: "Alamofire",
-      version: "4.0.0",
+      version: "4.5.0",
       previous_version: "3.0.0",
       requirements: [{
-        requirement: "~> 4.0.0",
+        requirement: "~> 4.5.0",
         file: "Podfile",
         source: nil,
         groups: []
@@ -133,7 +154,7 @@ RSpec.describe Dependabot::CocoaPods::FileUpdater do
         let(:podfile_body) do
           fixture("cocoapods", "podfiles", "version_specified")
         end
-        its(:content) { is_expected.to include "'Alamofire', '~> 4.0.0'" }
+        its(:content) { is_expected.to include "'Alamofire', '~> 4.5.0'" }
         its(:content) { is_expected.to include "'Nimble', '~> 2.0.0'" }
       end
 
@@ -155,7 +176,7 @@ RSpec.describe Dependabot::CocoaPods::FileUpdater do
         end
 
         it "locks the updated pod to the latest version" do
-          expect(file.content).to include "Alamofire (4.0.1)"
+          expect(file.content).to include "Alamofire (4.5.1)"
         end
 
         it "doesn't change the version of the other (also outdated) pod" do
@@ -175,26 +196,39 @@ RSpec.describe Dependabot::CocoaPods::FileUpdater do
         end
 
         it "locks the updated pod to the latest version" do
-          expect(file.content).to include "Alamofire (4.3.0)"
+          expect(file.content).to include "Alamofire (4.6.0)"
         end
       end
 
       context "with a git source for one of the other dependencies" do
         let(:podfile_body) { fixture("cocoapods", "podfiles", "git_source") }
 
+        before do
+          local_podspecs_path = File.join("tmp", "Local Podspecs")
+          FileUtils.mkdir_p(local_podspecs_path)
+
+          spec_fixtures_dir = File.join(fixtures_path, "podspecs")
+          alamofire_podspec = File.join(
+            spec_fixtures_dir,
+            "Alamofire-4.6.0.podspec.json"
+          )
+          FileUtils.cp(alamofire_podspec,
+                       File.join(local_podspecs_path, "Alamofire.podspec.json"))
+        end
+
         let(:dependency) do
           Dependabot::Dependency.new(
             name: "Nimble",
-            version: "6.0.0",
-            previous_version: "3.0.0",
+            version: "3.0.0",
+            previous_version: "2.0.0",
             requirements: [{
-              requirement: "~> 6.0.0",
+              requirement: "~> 3.0.0",
               file: "Podfile",
               source: nil,
               groups: []
             }],
             previous_requirements: [{
-              requirement: "~> 3.0.0",
+              requirement: "~> 2.0.0",
               file: "Podfile",
               source: nil,
               groups: []
@@ -204,7 +238,7 @@ RSpec.describe Dependabot::CocoaPods::FileUpdater do
         end
 
         it "locks the updated pod to the latest version" do
-          expect(file.content).to include "Nimble (6.0.1)"
+          expect(file.content).to include "Nimble (3.0.0)"
         end
 
         it "leaves the other (git referencing) pod alone" do
@@ -214,7 +248,7 @@ RSpec.describe Dependabot::CocoaPods::FileUpdater do
 
         it "generates the correct podfile checksum" do
           expect(file.content).
-            to include "CHECKSUM: 2db781eacbb9b29370b899ba5cd95a2347a63bd4"
+            to include "CHECKSUM: 2df7e373e023da06ffbeb508011feff582312fc6"
         end
 
         it "doesn't leave details of the access token in the lockfile" do
